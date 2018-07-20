bad056e74cb94f789edb6fc5aaafb543512d8257
dart2112 <DNAblue2112@gmail.com>
2018-07-20 22:31:07 +0930

    Initial commit

---
+++ src/main/java/net/lapismc/homespawn/HomeSpawn.java
@@ -0,0 +1,16 @@
+package net.lapismc.homespawn;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class HomeSpawn extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
